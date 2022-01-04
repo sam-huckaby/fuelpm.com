500cb75811724d84e2d84ffc17956b0eb162538e
Sam Huckaby <samhuckaby@gmail.com>
2022-01-04 11:04:22 -0700

    [FEATURE] Rework the footer to make more sense

--- pages/index.js
+++ pages/index.js
@@ -84,18 +84,21 @@ export default function Home() {
         {renderConcept(concept)}
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/netlify.svg" alt="Netlify Logo" className="h-6 ml-2" />
-          {' '}{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
+      <footer className="flex items-center justify-center w-full h-24 border-t flex-col">
+        <div className="netlify-container flex flex-row">
+          Hosted on{' '}<img src="/netlify.svg" alt="Netlify Logo" className="h-6 ml-2" />
+        </div>
+        <div className="vercel-container">
+          <a
+            className="flex items-center justify-center"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by{' '}
+            <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          </a>
+        </div>
       </footer>
     </div>
   )
